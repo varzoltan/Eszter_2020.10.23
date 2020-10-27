74744b74d62681e4b0e40b3f3d89dc23b174e72a
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-10-27 12:03:06 +0100

    12:03

--- Eszter_2020.10.23/Program.cs
+++ Eszter_2020.10.23/Program.cs
@@ -9,15 +9,16 @@ namespace Eszter_2020._10._23
 {
     class Program
     {
-        
+        static string[] kiserlet = new string[1000000];
+
         static void Main(string[] args)
         {
             //Fájl megnyítása
             //Létrehozok egy tömböt, amibe a fájlban lévő értékeket teszem.
-            string[] kiserlet = new string[1000000];
-            StreamReader olvas = new StreamReader(@"C:\Users\Rendszergazda\Downloads\kiserlet.txt");
+            //string[] kiserlet = new string[1000000];
+            //StreamReader olvas = new StreamReader(@"C:\Users\Rendszergazda\Downloads\kiserlet.txt");
             //Itt isjuk meg a megnyitás és bezárás között a programunkat!
-            int szamlalo = 0;
+            /*int szamlalo = 0;
             while (!olvas.EndOfStream)
             {
                 string sor = olvas.ReadLine();
@@ -32,7 +33,35 @@ namespace Eszter_2020._10._23
             {
                 Console.WriteLine(kiserlet[i]);
             }
+            */
+            Beolvas(@"C:\Users\Rendszergazda\Downloads\kiserlet.txt");
+            Kiiras();
             Console.ReadKey();
+        }
+
+        static void Beolvas(string eleresi_ut)
+        {
+            //string[] kiserlet = new string[1000000];//Inicializáltuk a tómböt.
+            StreamReader beolvas = new StreamReader(eleresi_ut);
+            int szamlalo = 0;
+            while (!beolvas.EndOfStream)
+            {
+                string sor = beolvas.ReadLine();
+                kiserlet[szamlalo] = sor;
+                szamlalo++;
+            }
+            beolvas.Close();
+            Console.WriteLine("Beolvasás kész!!!");
+        }
+
+        static void Kiiras()
+        {
+            int szamlalo = 0;
+            while (szamlalo < kiserlet.Length)
+            {
+                Console.WriteLine(kiserlet[szamlalo]);
+                szamlalo++;
+            }
         }
     }
 }
